e35cb3879d68cf1aa0ceb2867cd4e9dcc2f51710
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-07-14 20:51:19 -0500

    Add announcement module for retrieving announcements from Canvas API

---
+++ src/canvasAPI/announcement/__init__.py
@@ -0,0 +1,11 @@
+"""Announcement-related Canvas API modules
+
+Provides APIs for retrieving announcements.
+"""
+
+from .announcements import AnnouncementsAPI, announcements
+
+__all__ = [
+    "AnnouncementsAPI",
+    "announcements",
+]
