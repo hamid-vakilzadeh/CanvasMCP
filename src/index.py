ae2bfe25184a67c4a8e4cb047655342c1cd3cc39
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-07-23 19:36:36 -0500

    Refactor ModuleTools: Integrate get_user_token for base_url and access_token retrieval in module operations

--- src/index.py
+++ src/index.py
@@ -1,11 +1,12 @@
 from fastmcp import FastMCP
 from tools.courses import CourseTools
+from tools.modules import ModuleTools
 
 mcp = FastMCP("Canvas-MCP", stateless_http=True)
 
 
 CourseTools(mcp)
-
+ModuleTools(mcp)
 
 if __name__ == "__main__":
     mcp.run(transport="http", host="0.0.0.0", port=3000, path="/mcp")
