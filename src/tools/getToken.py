ed74b6e0e637abb593db7d3163f3d1f1d6c8ec6b
Hamid Vakilzadeh <vakilzas@uww.edu>
2025-07-31 23:48:02 -0500

    Remove unused import of random from nacl.utils in getToken.py

--- src/tools/getToken.py
+++ src/tools/getToken.py
@@ -8,7 +8,6 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from nacl.secret import SecretBox
-from nacl.utils import random
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
